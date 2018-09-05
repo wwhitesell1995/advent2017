81f084826dc3caf326c0d3800828dd59bb774bb7
wwhitesell1995 <whitesell1995@gmail.com>
2018-09-05 09:40:50 -0400

    Day 22 Part 2 Formatting Changes

--- Rust/day_22_2/src/main.rs
+++ Rust/day_22_2/src/main.rs
@@ -25,7 +25,7 @@ fn read_file_to_string(filename: String) -> String {
 //Gets the grid of infected nodes from a string.
 fn get_node_grid(contents: &str) -> HashMap<[usize; 2], &str> {
     let mut grid: HashMap<[usize; 2], &str> = HashMap::new();
-    let mid_point=[5000000, 5000000];
+    let mid_point = [5000000, 5000000];
     let mut row_length = 0;
     let mut col_length = 0;
 
@@ -38,7 +38,7 @@ fn get_node_grid(contents: &str) -> HashMap<[usize; 2], &str> {
         col_length += 1;
     }
 
-    let mut curr_row = mid_point[1]-(col_length / 2);
+    let mut curr_row = mid_point[1] - (col_length / 2);
 
     for line in contents.lines() {
         let mut curr_col = mid_point[0] - (row_length / 2);
@@ -65,33 +65,29 @@ fn get_num_bursts_infected(node_grid: &HashMap<[usize; 2], &str>) -> i32 {
     let mut curr_y = start_point[1] as i32;
 
     for _i in 0..10000000 {
-
         let curr_x_usize = curr_x as usize;
         let curr_y_usize = curr_y as usize;
-        burst_grid.entry([curr_y_usize, curr_x_usize]).or_insert(".");
+        burst_grid
+            .entry([curr_y_usize, curr_x_usize])
+            .or_insert(".");
 
-        if burst_grid[&[curr_y_usize, curr_x_usize]] == "."  {
+        if burst_grid[&[curr_y_usize, curr_x_usize]] == "." {
             curr_direction -= 1;
             if curr_direction < 0 {
                 curr_direction += num_states;
             }
 
             burst_grid.insert([curr_y_usize, curr_x_usize], "W");
-        }
-        else if burst_grid[&[curr_y_usize, curr_x_usize]] == "W"
-        {
-           num_infected += 1;
-           burst_grid.insert([curr_y_usize, curr_x_usize], "#");
-        }
-        else if burst_grid[&[curr_y_usize, curr_x_usize]] == "#" {
+        } else if burst_grid[&[curr_y_usize, curr_x_usize]] == "W" {
+            num_infected += 1;
+            burst_grid.insert([curr_y_usize, curr_x_usize], "#");
+        } else if burst_grid[&[curr_y_usize, curr_x_usize]] == "#" {
             curr_direction += 1;
             burst_grid.insert([curr_y_usize, curr_x_usize], "F");
-        }
-        else if burst_grid[&[curr_y_usize, curr_x_usize]] == "F"  {
+        } else if burst_grid[&[curr_y_usize, curr_x_usize]] == "F" {
             curr_direction += 2;
             burst_grid.insert([curr_y_usize, curr_x_usize], ".");
         }
-
 
         let curr_index = (curr_direction % num_states) as usize;
         if states[curr_index] == "left" {
